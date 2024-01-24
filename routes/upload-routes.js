aa72584fc97a51931c4628d72481fd805567c584
afyqeyzar <norafyqe@gmail.com>
2024-01-24 15:51:48 +0000

    feature: upload file endpoint saves on server

--- routes/upload-routes.js
+++ routes/upload-routes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
-// const userController = require("../controllers/user-controller");
+const path = require("path");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+// const upload = multer({ dest: "uploads/" });
 
 router.post("/", upload.single("file"), (req, res) => {
   res.send("upload successful");

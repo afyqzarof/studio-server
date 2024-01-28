8ed1712f4e83932aea413bd23b68413b850b72be
afyqeyzar <norafyqe@gmail.com>
2024-01-28 14:07:54 +0000

    chore: seed data

--- seed-data/pins.js
+++ seed-data/pins.js
@@ -38,4 +38,156 @@ module.exports = [
     x_coord: 0,
     y_coord: 0,
   },
+
+  {
+    board_id: 2,
+    width: 596,
+    height: 596,
+    id: "-cFgaF47WA",
+    type: "ImageNode",
+    x_coord: 512,
+    y_coord: 270,
+    data: {
+      file: "1706444532897.jpeg",
+    },
+  },
+  {
+    board_id: 2,
+    width: 664,
+    height: 467,
+    id: "sNmHdBcFPy",
+    type: "YoutubeVidNode",
+    x_coord: -326,
+    y_coord: -376,
+    data: {
+      youtube_id: "dHUq9xJcaZs",
+    },
+  },
+  // {
+  //   board_id: 2,
+  //   width: 338,
+  //   height: 570,
+  //   id: "4KZozsrb40",
+  //   type: "TextNode",
+  //   data: {
+  //     text: "your oversize clothes\nthe smile that you show\nme when\nu laugh\n\nyou obsession with food\nif i only could \nshare it \nwith you\n\ngoing walks\nyou giving your talks\non why and how\n\nand believing its true\ndon’t know how i found you\n(in this life)",
+  //   },
+
+  //   x_coord: 412,
+  //   y_coord: -367,
+  // },
+
+  {
+    board_id: 2,
+    width: 192,
+    height: 235,
+    id: "qjPTLOFQgz",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#1beece",
+    },
+
+    x_coord: -248,
+    y_coord: 458,
+  },
+  {
+    board_id: 2,
+    width: 192,
+    height: 235,
+    id: "Re5lcAPv9U",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#ce1bee",
+    },
+
+    x_coord: -11,
+    y_coord: 460,
+  },
+
+  {
+    board_id: 2,
+    width: 218,
+    height: 245,
+    id: "slQ8bqjNiT",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#eece1d",
+    },
+
+    x_coord: -249,
+    y_coord: 151,
+  },
+
+  {
+    board_id: 3,
+    width: 582,
+    height: 721,
+    id: "WAV6IwtZiJ",
+    type: "ImageNode",
+    data: {
+      file: "1706445605365.jpeg",
+    },
+    x_coord: 881,
+    y_coord: 548,
+  },
+  {
+    board_id: 3,
+    width: 192,
+    height: 235,
+    id: "_dZbMA53TD",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#30fde2",
+    },
+    x_coord: 1737.2404060913705,
+    y_coord: 180.64284263959394,
+  },
+  {
+    board_id: 3,
+    width: 192,
+    height: 235,
+    id: "ga3lCXN6Q-",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#e230fd",
+    },
+    x_coord: 1543.9474111675129,
+    y_coord: 172.0631472081219,
+  },
+  {
+    board_id: 3,
+    width: 192,
+    height: 235,
+    id: "wZoeXPH_nA-copy",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#30fd7b",
+    },
+    x_coord: 1341.9390862944163,
+    y_coord: 172.9409137055838,
+  },
+  {
+    board_id: 3,
+    width: 596,
+    height: 816,
+    id: "0WS0qnJicp",
+    type: "ImageNode",
+    data: {
+      file: "1706445392414.jpeg",
+    },
+    x_coord: 192.80160310677888,
+    y_coord: 201.48413378176298,
+  },
+  {
+    board_id: 3,
+    width: 192,
+    height: 235,
+    id: "hwunvYLOFa",
+    type: "ColorSelectorNode",
+    data: {
+      color: "#fde230",
+    },
+    x_coord: 859.1025380710659,
+    y_coord: 172.6946192893401,
+  },
 ];

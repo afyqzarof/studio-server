ef9e68a37e42bc072bacb15d26081475271fc34f
afyqeyzar <norafyqe@gmail.com>
2024-01-31 12:59:38 +0000

    wip: trying sqlite

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 
 module.exports = {
-  client: "mysql2",
+  // client: "mysql2",
+  // connection: {
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   charset: "utf8",
+  // },
+  client: "sqlite3",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    filename: "./data.sqlite3",
   },
 };

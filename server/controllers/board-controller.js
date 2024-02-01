f87f0d68a51a7ad5b31b2926c04e4b9fd9377e36
afyqeyzar <norafyqe@gmail.com>
2024-02-01 14:57:27 +0000

    feat: clean up file structure

--- server/controllers/board-controller.js
+++ server/controllers/board-controller.js
@@ -1,4 +1,4 @@
-const knex = require("knex")(require("../knexfile"));
+const knex = require("knex")(require("../../db/knexfile"));
 const { nanoid } = require("nanoid");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,4 +1,4 @@
-const knex = require("knex")(require("../knexfile"));
+const knex = require("knex")(require("../../db/knexfile"));
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 

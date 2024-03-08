e7693c4aee3d33add2ee175d2eb6ab878f59c67b
afyqeyzar <norafyqe@gmail.com>
2024-03-08 15:23:40 +0000

    wip: convert to typescript

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const port = process.env.PORT;
 const baseUrl = process.env.BASE_URL;
-const app = require("./server/app");
+const app = require("./src/server/app");
 
 app.listen(port, () => {
   console.log(`listening on  ${baseUrl + port}`);

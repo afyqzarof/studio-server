06b206b0cc1e6e6e2c82650cc4cfcc0c64931a77
afyqeyzar <norafyqe@gmail.com>
2024-03-08 17:28:32 +0000

    refactor: word routes to typescript

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import userRoutes from "./routes/user.routes";
 // import boardRoutes from "./routes/board-routes.js";
-// import wordRoutes from "./routes/word-routes.js";
+import wordRoutes from "./routes/word.routes";
 // import uploadRoutes from "./routes/upload-routes.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/upload", express.static("server/public/uploads"));
-app.use("/thumbnails", express.static("server/public/thumbnails"));
+app.use("/upload", express.static(path.resolve(__dirname, "public/uploads")));
+app.use(
+  "/thumbnails",
+  express.static(path.resolve(__dirname, "public/thumbnails"))
+);
 app.use("/users", userRoutes);
 // app.use("/boards", boardRoutes);
-// app.use("/word", wordRoutes);
+app.use("/word", wordRoutes);
 // app.use("/upload", uploadRoutes);
 
 export default app;

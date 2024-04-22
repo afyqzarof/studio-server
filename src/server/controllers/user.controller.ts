d28d808236cd489eb408affbbfcc20700e8a9a14
kabirseth4 <kabirseth4@gmail.com>
2024-04-22 14:45:29 +0100

    fix: change all endpoint generic error responses to status 500

--- src/server/controllers/user.controller.ts
+++ src/server/controllers/user.controller.ts
@@ -64,7 +64,7 @@ const register = async (req: Request, res: Response) => {
     return res.status(201).json({ username, email });
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ message: "Failed registration" });
+    return res.status(500).json({ message: "Failed registration" });
   }
 };
 
